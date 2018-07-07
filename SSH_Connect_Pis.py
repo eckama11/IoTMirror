1f3aaead908d7d8dcd7b10a6687578da2139fe37
Amanda Eckhardt <eckama11@gmail.com>
2018-07-07 17:56:03 -0500

    ssh
    using pexpect, pxssh instead of subporocess

--- SSH_Connect_Pis.py
+++ SSH_Connect_Pis.py
@@ -2,6 +2,8 @@
 
 import subprocess
 import sys
+from pexpect import pxssh
+
 
 with open('network_info.py', 'r')as f:
         for line in f:
@@ -14,16 +16,27 @@ for i, value in enumerate(network_info):
         network_info[i] = value.replace('(', '').replace(')', '')
 print network_info[0]
 
-HOST=network_info[0]
-COMMAND="uname -a"
+#HOST=network_info[0]
+#COMMAND="uname -a"
 
-ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
-	shell=False,
-	stdout=subprocess.PIPE,
-	stderr=subprocess.PIPE)
-result = ssh.stdout.readline()
-if result == []:
-	error = ssh.stderr.readlines()
-	print >>sys.stderr, "ERROR: %s" % error
+#ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
+#	shell=False,
+#	stdout=subprocess.PIPE,
+#	stderr=subprocess.PIPE)
+#result = ssh.stdout.readline()
+#if result == []:
+#	error = ssh.stderr.readlines()
+#	print >>sys.stderr, "ERROR: %s" % error
+#else:
+#	print result
+
+s = pxssh.pxssh()
+if not s.login (network_info[0], 'pi', 'raspberry'):
+    print "SSH session failed on login."
+    print str(s)
 else:
-	print result
+    print "SSH session login successful"
+    s.sendline ('ls -l')
+    s.prompt()         # match the prompt
+    print s.before     # print everything before the prompt.
+    s.logout()
